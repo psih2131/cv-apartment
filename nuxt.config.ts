7b700692de230d0c4d996b88dfb4d753385d83e4
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-08-15 16:29:35 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'apartments',
+      htmlAttrs: {
+        lang: 'ru',
+      },
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
